db00925541a1e0106f1bd3bf68d0630ee1e1c264
Jen Looper <jen.looper@gmail.com>
2019-04-05 14:43:19 -0400

    neating app.js

--- app/app.js
+++ app/app.js
@@ -5,25 +5,12 @@ import Home from './components/Home';
 // Uncommment the following to see NativeScript-Vue output logs
 //Vue.config.silent = false;
 
-import { TNSFontIcon, fonticon } from './nativescript-fonticon';
+import { ModalStack, overrideModalViewMethod, VueWindowedModal } from './nativescript-windowed-modal';
 
-//TNSFontIcon.debug = true;
+overrideModalViewMethod();
+Vue.registerElement('ModalStack', () => ModalStack);
+Vue.use(VueWindowedModal);
 
-TNSFontIcon.paths = {
-    fa: './assets/font-awesome.css',
-};
-TNSFontIcon.loadCss();
-
-import { ModalStack, overrideModalViewMethod, VueWindowedModal } from "./nativescript-windowed-modal"
-
-overrideModalViewMethod()
-Vue.registerElement("ModalStack", () => ModalStack)
-Vue.use(VueWindowedModal)
-
-Vue.filter('fonticon', fonticon);
-Vue.config.silent = true
 new Vue({
-
-    render: h => h('frame', [h(Home)])
-
+	render: h => h('frame', [h(Home)]),
 }).$start();
